e32dd70d8cef693a07b51db418d2b877055701f6
Erica Sun <ericasun@seas.upenn.edu>
2020-08-21 14:12:06 +0000

    change profile background

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,53 +4,59 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
-
+$(document).ready(function () {
   // form slides down and autofocuses when nav button is pressed
-  $('.nav-button').on('click', function(event) {
+  $(".nav-button").on("click", function (event) {
     event.preventDefault();
-    $('.new-tweet').slideToggle();
-    $('#tweet-text').focus();
+    $(".new-tweet").slideToggle();
+    $("#tweet-text").focus();
   });
 
   // ajax request triggered by submit event
-  $('form').on('submit', function(event) {
+  $("form").on("submit", function (event) {
     event.preventDefault();
-    $('#empty').slideUp().delay(300);
-    $('#long').slideUp().delay(300);
+    $("#empty").slideUp().delay(300);
+    $("#long").slideUp().delay(300);
     // validate form
-    if (!$('#tweet-text').val() || $('#tweet-text').val().trim().length === 0) {
-      $('#empty').slideDown();
-    } else if ($('.counter').val() < 0) {
-      $('#long').slideDown();
+    if (!$("#tweet-text").val() || $("#tweet-text").val().trim().length === 0) {
+      $("#empty").slideDown();
+    } else if ($(".counter").val() < 0) {
+      $("#long").slideDown();
     } else {
-      $.ajax({url: '/tweets', method: 'POST', data: $(this).serialize(), dataType: 'text'}).then(function(response) {
+      $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data: $(this).serialize(),
+        dataType: "text",
+      }).then(function (response) {
         console.log("Finished");
         // trigger ajax response
         loadTweets();
         // clear textbox and reset counter
-        $('#tweet-text').val('');
-        $('.counter').val(140);
+        $("#tweet-text").val("");
+        $(".counter").val(140);
       });
     }
   });
 
   // ajax response triggered by ajax request and on page load
-  const loadTweets = function() {
-    $.ajax({url: '/tweets', method: 'GET', dataType: 'JSON'}).then(function(response) {
+  const loadTweets = function () {
+    $.ajax({ url: "/tweets", method: "GET", dataType: "JSON" }).then(function (
+      response
+    ) {
       renderTweets(response);
     });
   };
 
   // call createTweetElement on each json object in array
-  const renderTweets = function(tweets) {
-    tweets.forEach(tweet => {
-      $('.old-tweets').prepend(createTweetElement(tweet));
+  const renderTweets = function (tweets) {
+    tweets.forEach((tweet) => {
+      $(".old-tweets").prepend(createTweetElement(tweet));
     });
   };
 
   // create tweet element given tweet object
-  const createTweetElement = function(tweetObj) {
+  const createTweetElement = function (tweetObj) {
     const $tweet = $(`<article>
       <header class="tweet-header">
         <div class="avatar-name">
@@ -73,20 +79,15 @@ $(document).ready(function() {
   };
 
   // helper function to convert unix time into relative time from now
-  const escape =  function(str) {
-    let div = document.createElement('div');
+  const escape = function (str) {
+    let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
-
   // load tweets and hide error messages on page load
   loadTweets();
-  $('#empty').hide();
-  $('#long').hide();
-  $('.new-tweet').hide();
-  
+  $("#empty").hide();
+  $("#long").hide();
+  $(".new-tweet").hide();
 });
-
-
-
